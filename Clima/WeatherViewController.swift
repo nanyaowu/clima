df9070f22d52766cd854abd5d7e3d7a28a99730d
Nan-Yao Wu <nanyao.h.wu@gmail.com>
2019-02-23 23:24:12 +0800

    CocoaPods and location

--- Clima/WeatherViewController.swift
+++ Clima/WeatherViewController.swift
@@ -7,18 +7,18 @@
 //
 
 import UIKit
+import CoreLocation
 
-
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     
     //Constants
     let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
-    let APP_ID = "e72ca729af228beabd5d20e3b7749713"
+    let APP_ID = "166b2c25ecfc050830f9247708909822"
     /***Get your own App ID at https://openweathermap.org/appid ****/
     
 
     //TODO: Declare instance variables here
-    
+    private let locationManager = CLLocationManager()
 
     
     //Pre-linked IBOutlets
@@ -32,7 +32,10 @@ class WeatherViewController: UIViewController {
         
         
         //TODO:Set up the location manager here.
-    
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
         
         
     }
@@ -76,11 +79,24 @@ class WeatherViewController: UIViewController {
     
     
     //Write the didUpdateLocations method here:
-    
-    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[locations.count - 1]
+        if location.horizontalAccuracy > 0 {
+            locationManager.stopUpdatingLocation()
+            print("longitude = \(location.coordinate.longitude), latitude = \(location.coordinate.latitude).")
+        }
+        
+        let latitude = String(location.coordinate.latitude)
+        let longitude = String(location.coordinate.longitude)
+        let params: [String : String] = ["lat": latitude, "lon": longitude, "appid": APP_ID]
+        
+    }
     
     //Write the didFailWithError method here:
-    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+        cityLabel.text = "Location Unavailable"
+    }
     
     
 
